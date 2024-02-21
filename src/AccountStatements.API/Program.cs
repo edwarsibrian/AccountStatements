b26cc65d83b6ded53f7b69711864a8e4e5d9df81
AccountsStatements <edward_el_doc@hotmail.com>
2024-02-21 00:36:45 -0600

    cambio estructura directorios

--- src/AccountStatements.API/Program.cs
+++ src/AccountStatements.API/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
